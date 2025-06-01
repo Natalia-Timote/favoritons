b6578b4d1d1c221deeb3694bd7b4f738cd66bf7b
Natalia Timote <nataliamirian.nt@gmail.com>
2025-06-01 20:28:08 -0300

    criando a lista de cards com base no json

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,7 @@ import Banner from "components/Banner";
 import Title from "components/Title";
 import { TiHeart } from "react-icons/ti";
 import Card from "components/Card";
+import cartoons from "json/db.json";
 
 export default function Home() {
     return (
@@ -15,12 +16,9 @@ export default function Home() {
                 <h1>Um lugar para guardar seus desenhos favoritos! <TiHeart /></h1>
             </Title>
             <section className={styles.cards}>
-            <Card 
-                title="As meninas super-poderosas" 
-                date="1998" 
-                description="Acidentalmente criadas em laboratório por um professor, três irmãs combatem os malfeitores em Townsville." 
-                image="/images/cartoons/as-meninas-super-poderosas.png" 
-            />
+                {cartoons.map(cartoon => {
+                    return <Card {...cartoon} key={cartoon.id} />
+                })}
             </section>
             <Footer />
         </>
